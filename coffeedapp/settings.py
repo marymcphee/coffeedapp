b91ef77718040f318500ac70deea652cc77c96d4
marymcphee <taftmcphee@gmail.com>
2015-05-13 22:05:58 -0400

    base dir for templates in settings.py

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coffeedapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,9 +109,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (
-    os.path.join(MAIN_DIR, 'templates'),
-    )
 
 STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
